7a36b295facbaf9efaf29adfeffeec92d07d7d91
Griffin Smith <griffinlsmith@arizona.edu>
2025-09-09 22:12:34 -0700

    First Monster Commit

--- cubic_solver.py
+++ cubic_solver.py
@@ -5,10 +5,145 @@ def solve_cubic(a, b, c, d):
     Returns list of 1..3 roots (complex if needed).
     """
     roots = []
-    # ################################################################
-    # Your solution goes here
-    # ################################################################
+    # Going to try and handle some degenerate cases...
+    if d == 0:
+        # We can factor an x out and reduce to a quadratic...
+        roots.append(0)
+        quadRoots = solve_quad(a,b,c)
+        roots.append(quadRoots[0])
+        roots.append(quadRoots[1])
+        return roots
+    
+    if a == 0:
+        # This is not even a real cubic...
+        # This is a quadratic problem...
+        # I will solve it for you though...
+        print('User has entered a degenerate cubic...')
+        roots = quadRoots(b,c,d)
+        return roots
+
+    if (b == 0) & (c == 0):
+        # This is really a roots of unity problem...
+        c3 = 1
+        c0 = d/a
+        if c0<0:
+            crho = (math.abs(c0))**(1/3)
+            phase0 = 0
+            phase1 = (2/3)*math.pi
+            phase2 = (4/3)*math.pi
+            j = 0+1j
+            roots.append(crho*cmath.exp(j*phase0))
+            roots.append(crho*cmath.exp(j*phase1))
+            roots.append(crho*cmath.exp(j*phase2))
+            return roots
+        else:
+            phi = (-1/3)*cmath.pi
+            crho = (math.abs(c0))**(1/3)
+            phase0 = phi
+            phase1 = phi + (2/3)*cmath.pi
+            phase2 = phi + (4/3)*cmath.pi
+            j = 0+1j
+            roots.append(crho*cmath.exp(j*phase0))
+            roots.append(crho*cmath.exp(j*phase1))
+            roots.append(crho*cmath.exp(j*phase2))
+            return roots
+    
+    # Ok.... so we have a somewhat interesting cubic if we reach this point...
+    # I am going to divide through by leading coeff...
+    c3 = 1
+    c2 = b/a
+    c1 = c/a
+    c0 = d/a
+    # Now we depress it into t^3 + pt + q
+    p = (-1*(c2)**2)/3 + c1
+    q = (2*(c3**3)/27) -(c1*c2/3) + c0
+
+    # Here is a helper term...
+    
+    if p<=0:
+        a = (-2/3)*p*(p*p/9)**(0.25)
+        threeTheta = cmath.acos(-q/a)
+        theta1 = threeTheta/3
+        t1 = cmath.cos(theta1)*2*(p*p/9)**(0.25)
+        theta2 = theta1 + (2/3)*cmath.pi
+        t2 = cmath.cos(theta2)*2*(p*p/9)**(0.25)
+        theta3 = theta1 - (2/3)*cmath.pi
+        t3 = cmath.cos(theta3)*2*(p*p/9)**(0.25)
+    else:
+        a = (2/3)*p*(p*p/9)**(0.25)
+        threeTheta = cmath.asinh(-q/a)
+        theta1 = threeTheta/3
+        t1 = cmath.sinh(theta1)*2*(p*p/9)**(0.25)
+        theta2 = theta1 + (2/3)*cmath.pi*(0+1j)
+        t2 = cmath.sinh(theta2)*2*(p*p/9)**(0.25)
+        theta3 = theta1 - (2/3)*cmath.pi*(0+1j)
+        t3 = cmath.sinh(theta3)*2*(p*p/9)**(0.25)
+    
+    roots.append(t1-c2/3)
+    roots.append(t2-c2/3)
+    roots.append(t3-c2/3)
     return roots
+    
+
+
+
+def solve_linear(a,b):
+    roots = [-b/a]
+    return roots
+
+def solve_quad(a,b,c):
+    roots = []
+    if a == 0:
+        # This is just a linear problem
+        roots = solve_linear(b,c)
+        return roots
+    if c == 0:
+        # We can factor out a zero...
+        roots.append(0)
+        roots.append(solve_linear(a,b))
+        return roots
+    # Now we should have a non-trivial quadratic...
+    if a != 1:
+        # We want to recondition...
+        c0 = c/a
+        c1 = b/a
+        c2 = 1
+
+    discrim = c1**2 - 4*c0
+
+    if discrim == 0:
+        roots.append(-1*c1/2)
+        roots.append(-1*c1/2)
+        return roots
+    
+    if discrim < 8 & discrim >0:
+        # We should be able to hit it with arcos
+        alpha1 = math.acos((c1**2 -4*c0-4)/4)
+        alpha2 = 2*math.pi - alpha1
+
+        alpha1/=2
+        alpha2/=2
+
+        y1 = math.cos(alpha1)
+        y2 = math.cos(alpha2)
+
+        t1 = 4**(0.25)*y1
+        t2 = 4**(0.25)*y2
+
+        roots.append(t1 - (c1/2))
+        roots.append(t2 - (c1/2))
+        return roots
+    else:
+        alpha1 = math.acosh((c1**2 -4*c0-4)/4)
+        alpha1/=2
+        y1 = math.cosh(alpha1)
+        t1 = 4**(0.25)*y1
+        roots.append(t1-(c1/2))
+        roots.append((c0/(roots[0])))
+        return roots
+
+        
+
 
 
 def main():
--- quartic_solver.py
+++ quartic_solver.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cubic_solver
+import math
+import cmath
 
 def solve_quartic(a, b, c, d, e):
     """Solve a*x^4 + b*x^3 + c*x^2 + d*x + e = 0.
@@ -8,12 +11,78 @@ def solve_quartic(a, b, c, d, e):
     """
     roots = []
 
-    # ################################################################
-    # Your solution goes here
-    # ################################################################
+    # Going to try and handle some edge cases first...
+    if e == 0:
+        # We can factor an x and turn into a cubic...
+        roots.append(0)
+        cubicRoots = cubic_solver.solve_cubic(a,b,c,d)
+        roots.append(cubicRoots[0])
+        roots.append(cubicRoots[1])
+        roots.append(cubicRoots[2])
+        return roots
+    
+    if a == 0:
+        # This is not a quartic...
+        # I will solve it for you anyway...
+        roots = cubic_solver.solve_cubic(b,c,d,e)
+        return roots
+    
+    # Ok... lets get a leading coeff of 1 and depress the quartic...
+    c4 = 1
+    c3 = b/a
+    c2 = c/a
+    c1 = d/a
+    c0 = e/a
 
-    # Sample return statement
+    # Going to put it in the form t^4 +qt^2 +rt +s...
+    q = c2 - (3/8)*(c3**2)
+    r = c1 - (1/2)*c3*c2 + (1/8)*(c3**3)
+    s = c0 - (1/4)*c3*c1 + (1/16)*(c3**2)*c2 - (3/256)*(c3**4)
+
+    # If r is zero then this is actually a quadratic problem...
+    if r ==0:
+        quadRoots = cubic_solver.solve_quad(1,q,s)
+        r1 = quadRoots[0]
+        r2 = quadRoots[1]
+        multVec = [1,1,1,1]
+        if r1<0:
+            multVec[0] = 0+1j
+            multVec[1] = multVec[0]
+        if r2<0:
+            multVec[2] = 0+1j
+            multVec[3] = multVec[2]
+        root1 = (-1)*(r1*r1)**(0.25) - (c3/4)
+        root2 = (r1*r1)**(0.25) - (c3/4)
+        root3 = (-1)*(r2*r2)**(0.25) - (c3/4)
+        root4 = (r2*r2)**(0.25) - (c3/4)
+        roots.append(root1*multVec[0])
+        roots.append(root2*multVec[1])
+        roots.append(root3*multVec[2])
+        roots.append(root4*multVec[3])
+        return roots
+    
+    # Now we have to construct the resolvent cubic...
+    resolveCoeff = [8,8*q,(2*q*q - 8*s),-1*r*r]
+    resolveCubicRoots = cubic_solver.solve_cubic(resolveCoeff[0],resolveCoeff[1],resolveCoeff[2],resolveCoeff[3])
+    # One of the roots have to be real....
+    for root in resolveCubicRoots:
+        if math.abs(root.imag)<(10**(-6)):
+            chosenRoot = root
+            break
+    
+    linearTerm = (4*chosenRoot*chosenRoot)**(0.25)
+    quad1Coeff = [1,-1*linearTerm,(q/2) + chosenRoot + (r/(2*linearTerm))]
+    quad2Coeff = [1,linearTerm,(q/2) + chosenRoot - (r/(2*linearTerm))]
+
+    qroot1 = cubic_solver.solve_quad(quad1Coeff[0],quad1Coeff[1],quad1Coeff[2])
+    qroot2 = cubic_solver.solve_quad(quad2Coeff[0],quad2Coeff[1],quad2Coeff[2])
+
+    roots.append(qroot1[0] - (c3/4))
+    roots.append(qroot1[1] - (c3/4))
+    roots.append(qroot2[0] - (c3/4))
+    roots.append(qroot2[1] - (c3/4))
     return roots
+
 
 
 def main():
